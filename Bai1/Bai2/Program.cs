dd652ef628e7d799a701b101a50ed4975d739423
Lehoainam12345 <lehoainam21112006@gmail.com>
2026-03-18 23:54:30 +0700

    làm bài 2

---
+++ Bai1/Bai2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("\t");
+            Console.WriteLine("Họ và tên: Lê Hoài Nam- MSV: 2415053122327");
+            Console.WriteLine("Bài 2: Tính tổng các phần tử");
+            List<int> list = new List<int>();
+            int tg = 0;
+            Console.Write("Nhập số phần tử muốn có trong list: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Nhập phần tử thứ {0} ", i + 1);
+                int a = int.Parse(Console.ReadLine());
+                list.Add(a);
+            }
+            Console.WriteLine("Phần tử trong mảng vừa nhập là: ");
+            foreach (int a in list)
+            {
+                Console.Write("{0} ", a);
+            }
+            Console.WriteLine("\t");
+            foreach (int a in list)
+            {
+                tg = tg + a;
+            }
+            Console.Write("Tổng các phần tử trong mảng vừa nhập: ");
+            Console.WriteLine(tg);
+            Console.ReadLine();
+
+        }
+    }
+}
